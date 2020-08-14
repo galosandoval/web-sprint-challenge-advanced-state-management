ccd2655add9eb425a67027e2779cabe85c8c97b4
Galo Sandoval <galosan92@gmail.com>
2020-08-14 09:34:39 -0700

    displaying data

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,14 +5,15 @@ import App from "./components/App";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { reducer } from "./reducers";
+import { Provider } from "react-redux";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
